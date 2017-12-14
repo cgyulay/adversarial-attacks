31f2ae08481c9740bea2baf83e116c8d932bfb32
Colton Gyulay <coltongyulay@gmail.com>
2017-12-14 16:50:34 -0500

    experiments done

--- adversarial.py
+++ adversarial.py
@@ -9,21 +9,22 @@ from torchvision.models.inception import inception_v3
 
 from PIL import Image
 from scipy.misc import imsave
+from scipy.interpolate import spline
 
 import matplotlib.pyplot as plt
 import os
 import numpy as np
+from random import randint
 
 
 classes = eval(open('classes.txt').read())
 trans = T.Compose([T.ToTensor(), T.Lambda(lambda t: t.unsqueeze(0))])
 reverse_trans = lambda x: np.asarray(T.ToPILImage()(x))
 
-# eps = 2 * 8 / 225. 
-eps = 0.03
-steps = 40
+eps = 10  # 0.01
 norm = float('inf')
-step_alpha = 0.01 
+steps = 40
+step_alpha = 0.01  # 1
 
 model = inception_v3(pretrained=True, transform_input=True).cpu()
 loss = nn.CrossEntropyLoss()
@@ -41,6 +42,12 @@ def get_class(img):
     return classes[cls]
 
 
+def get_top_five(img):
+    x = Variable(img, volatile=True).cpu()
+    top5 = model(x).data.topk(5)[1].cpu().numpy()[0]
+    return [classes[cls] for cls in top5]
+
+
 def draw_result(img, noise, adv_img):
     fig, ax = plt.subplots(1, 3, figsize=(15, 10))
     orig_class, attack_class = get_class(img), get_class(adv_img)
@@ -49,11 +56,55 @@ def draw_result(img, noise, adv_img):
     ax[1].imshow(noise[0].cpu().numpy().transpose(1, 2, 0))
     ax[1].set_title('Attacking noise')
     ax[2].imshow(reverse_trans(adv_img[0]))
+    attack_class = ', '.join(attack_class.split(',')[:2])
     ax[2].set_title('Adversarial example: {}'.format(attack_class))
     for i in range(3):
         ax[i].set_axis_off()
     plt.tight_layout()
-    plt.show()
+
+    # plt.show()
+    fig.savefig('adv.png', dpi=fig.dpi)
+
+
+def graph_result(epsilons, y1, y2, y3, title):
+    fig = plt.figure()
+
+    # xnew = np.linspace(min(epsilons), max(epsilons), 20)
+    # y1_smooth = spline(epsilons, y1, xnew)
+    # l1, = plt.plot(xnew, y1_smooth, 'r--', label="fast")
+
+    l1, = plt.plot(epsilons, y1, 'r--', label="fast")
+    l2, = plt.plot(epsilons, y2, 'b--', label="iter non-target")
+    l3, = plt.plot(epsilons, y3, 'g--', label="iter target")
+
+    plt.xlabel('epsilon')
+    plt.ylabel('%s equivalence' % title)
+
+    plt.legend(handles=[l1, l2, l3])
+    # plt.show()
+
+    fig.savefig(title + '.png', dpi=fig.dpi)
+
+
+def fgsm(img):
+    img = img.cpu()
+    label = torch.zeros(1, 1).cpu()
+    
+    x, y = Variable(img, requires_grad=True), Variable(label)
+    zero_gradients(x)
+    out = model(x)
+    y.data = out.data.max(1)[1]
+    _loss = loss(out, y)
+    _loss.backward()
+    # normed_grad = eps * torch.sign(x.grad.data)
+    normed_grad = torch.sign(x.grad.data)
+    step_adv = x.data + normed_grad
+    adv = step_adv - img
+    adv = torch.clamp(adv, -eps, eps)
+    result = img + adv
+    result = torch.clamp(result, 0.0, 1.0)
+
+    return result.cpu(), adv.cpu()
 
 
 def non_targeted_attack(img):
@@ -97,11 +148,85 @@ def targeted_attack(img, label):
     return result.cpu(), adv.cpu()
 
 
-img = load_image('img/car1.jpg')
+# # load images from img dir
+# fnames = [f for f in os.listdir('img') if '.jpg' in f]
+# images = []
+# for f in fnames:
+#     img = load_image('img/' + f)
+#     images.append((f, img))
+
+# # compare different methods across eps
+# epsilons = [0.005, 0.08, 0.16, 0.24, 0.32, 0.4, 0.5]
+# n = 20
+# methods = ['fgsm', 'non_targ', 'targ']
+# y1_1 = []
+# y2_1 = []
+# y3_1 = []
+# y1_5 = []
+# y2_5 = []
+# y3_5 = []
+
+# for e in epsilons:
+#     eps = e
+
+#     for m in methods:
+#         tot1 = 0.0
+#         tot5 = 0.0
+
+#         for (f, img) in images[:n]:
+#             print 'img: %s, eps: %f, method: %s' % (f, eps, m)
+#             target = randint(0, 999)
+
+#             # generate adversarial images for all methods
+#             if m == 'fgsm':
+#                 adv_img, noise = fgsm(img)
+#             elif m == 'non_targ':
+#                 adv_img, noise = non_targeted_attack(img)
+#             else:
+#                 adv_img, noise = targeted_attack(img, target)
+
+#             # check if top class is equivalent
+#             orig_class, attack_class = get_class(img), get_class(adv_img)
+#             if orig_class == attack_class:
+#                 tot1 += 1
+
+#             # check if orig class in top 5
+#             top5 = get_top_five(adv_img)
+#             if orig_class in top5:
+#                 tot5 += 1
+
+#         tot1 /= n
+#         tot5 /= n
+
+#         if m == 'fgsm':
+#             y1_1.append(tot1)
+#             y1_5.append(tot5)
+#         elif m == 'non_targ':
+#             y2_1.append(tot1)
+#             y2_5.append(tot5)
+#         else:
+#             y3_1.append(tot1)
+#             y3_5.append(tot5)
+
+# # graph results of each method at each epsilon
+# print y1_1, y2_1, y3_1
+# graph_result(epsilons, y1_1, y2_1, y3_1, 'top-1')
+
+# print y1_5, y2_5, y3_5
+# graph_result(epsilons, y1_5, y2_5, y3_5, 'top-5')
+
+# fgsm
+# img = load_image('img/8.jpg')
+# adv_img, noise = fgsm(img)
+# draw_result(img, noise, adv_img)
+
+non-targeted
+img = load_image('img/97.jpg')
 adv_img, noise = non_targeted_attack(img)
 draw_result(img, noise, adv_img)
 
-
-# img = load_image('input1.jpg')
-# adv_img, noise = targeted_attack(img, 486)
+# targeted
+# target = randint(0, 999)
+# img = load_image('img/63.jpg')
+# adv_img, noise = targeted_attack(img, target)
 # draw_result(img, noise, adv_img)
